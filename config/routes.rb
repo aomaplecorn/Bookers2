81832cbc058880e3e50364bd3a4420b2adba0293
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-05-15 06:46:35 +0000

    [add]ログイン機能(devise)、Top/About画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  devise_for :users
+
+  root to: "home#top", as: 'top'
+  get 'home/about' => 'home#about',  as: 'about'
+
+
+
+
+
 end
