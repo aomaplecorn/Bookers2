322a4320298467828f2ff1675d42d4368678c160
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-05-16 13:11:11 +0000

    [add]Booksの一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-
   devise_for :users
 
   root to: "home#top", as: 'top'
   get 'home/about' => 'home#about',  as: 'about'
 
+  resources :books, only: [:create,:index,:show,:edit]
 
-
+  resources :users, only: [:index,:show,:edit]
 
 
 end
