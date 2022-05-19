9a8686fa0860c42db481fc98a1e69ff06fbdf60e
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-05-19 14:56:48 +0000

    [add]Bootstrapを使って各ビュー画面のデザイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: 'devise/registrations',
-                                    sessions: 'devise/sessions'}
+  devise_for :users
 
   root to: "home#top", as: 'top'
   get 'home/about' => 'home#about',  as: 'about'
